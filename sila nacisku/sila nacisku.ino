ecc659d8ea3208574d097939aa3ec9b05d07e561
sdassh <94625239+sdassh@users.noreply.github.com>
2023-01-26 11:55:46 +0100

    Update sila nacisku.ino
    Przystosowałem program do przesyłania danych drogą radiową

--- sila nacisku/sila nacisku.ino
+++ sila nacisku/sila nacisku.ino
@@ -1,25 +1,58 @@
-#define FORCE_SENSOR_PIN A0 // the FSR and 10K pulldown are connected to A0
+#include <CanSatKit.h>
+#define FORCE_SENSOR_PIN A1 // the FSR and 10K pulldown are connected to A0
+
+using namespace CanSatKit;
+
+int counter = 1;
+bool led_state = false;
+const int led_pin = 13;
+
+Radio radio(Pins::Radio::ChipSelect,
+            Pins::Radio::DIO0,
+            433.0,
+            Bandwidth_125000_Hz,
+            SpreadingFactor_9,
+            CodingRate_4_8);
+
+Frame frame;
 
 void setup() {
-  Serial.begin(115200);
+  SerialUSB.begin(115200);
+  pinMode(led_pin, OUTPUT);
+
+  radio.begin();
 }
 
 void loop() {
+
+  digitalWrite(led_pin, led_state);
+  led_state = !led_state;
+  
+  frame.print(counter);
+  counter++;
+  
   int analogReading = analogRead(FORCE_SENSOR_PIN);
 
-  Serial.print("Force sensor reading = ");
-  Serial.print(analogReading); // print the raw analog reading
+  
+
+  frame.print("Force sensor reading = ");
+  frame.print(analogReading); // print the raw analog reading
 
   if (analogReading < 10)       // from 0 to 9
-    Serial.println(" -> no pressure");
+    frame.println(" -> no pressure");
   else if (analogReading < 200) // from 10 to 199
-    Serial.println(" -> light touch");
+    frame.println(" -> light touch");
   else if (analogReading < 500) // from 200 to 499
-    Serial.println(" -> light squeeze");
+    frame.println(" -> light squeeze");
   else if (analogReading < 800) // from 500 to 799
-    Serial.println(" -> medium squeeze");
+    frame.println(" -> medium squeeze");
   else // from 800 to 1023
-    Serial.println(" -> big squeeze");
+    frame.println(" -> big squeeze");
+  
+  radio.transmit(frame);
+  SerialUSB.println(frame);
 
-  delay(200);
+  frame.clear();
+
+  delay(1000);
 }
