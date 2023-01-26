ea5bfe2b56d475853310542c8bdaaee17cf436ff
Krzychulee <120094140+Krzychulee@users.noreply.github.com>
2023-01-26 11:34:24 +0100

    Add files via upload

---
+++ miernik odleglosci/miernik odleglosci.ino
@@ -0,0 +1,22 @@
+#define trigPin 5
+#define echoPin 4
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(150);
+}
